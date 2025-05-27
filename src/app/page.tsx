9ed0f8f5f47677abac04cfeb323cf19e2d1b42a3
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-05-28 00:44:06 +0530

    Added Navbar component with responsive Desktop and Mobile navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-        <SignUpButton mode="modal">
-          <Button>Sign up</Button>
-        </SignUpButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
